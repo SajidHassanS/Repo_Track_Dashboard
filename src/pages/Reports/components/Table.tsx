b6a4f246b800e58d234442efc72a0752d28f027a
Haseeb Ali Chaudhary <haseebalichaudhary3@gmail.com>
2024-06-11 21:00:04 +0500

    Table columns added

--- src/pages/Reports/components/Table.tsx
+++ src/pages/Reports/components/Table.tsx
@@ -31,6 +31,14 @@ const Table: React.FC = () => {
     hitType: randomString(5),
     client: randomString(8),
     classification: randomString(8),
+    status: randomString(6),
+    location: randomString(10),
+    department: randomString(8),
+    priority: randomString(4),
+    assignedTo: randomString(10),
+    resolvedDate: randomDate(new Date(2020, 0, 1), new Date()).toDateString(),
+    resolutionType: randomString(5),
+    comments: randomString(12),
   }));
 
   return (
@@ -38,26 +46,44 @@ const Table: React.FC = () => {
       <table className="table-auto min-w-full">
         <thead>
           <tr className="bg-gray-200">
-            <th className="px-4 py-2">Case Number</th>
-            <th className="px-4 py-2">BSO Date</th>
-            <th className="px-4 py-2">Repo Date</th>
-            <th className="px-4 py-2">BSO Type</th>
-            <th className="px-4 py-2">Hit Type</th>
-            <th className="px-4 py-2">Client</th>
-            <th className="px-4 py-2">Classification</th>
+            <th className="px-4 py-2 whitespace-nowrap">Case Number</th>
+            <th className="px-4 py-2 whitespace-nowrap">BSO Date</th>
+            <th className="px-4 py-2 whitespace-nowrap">Repo Date</th>
+            <th className="px-4 py-2 whitespace-nowrap">BSO Type</th>
+            <th className="px-4 py-2 whitespace-nowrap">Hit Type</th>
+            <th className="px-4 py-2 whitespace-nowrap">Client</th>
+            <th className="px-4 py-2 whitespace-nowrap">Classification</th>
+            <th className="px-4 py-2 whitespace-nowrap">Debtor</th>
+            <th className="px-4 py-2 whitespace-nowrap">Year Make</th>
+            <th className="px-4 py-2 whitespace-nowrap">Day Worked</th>
+            <th className="px-4 py-2 whitespace-nowrap">VIN</th>
+            <th className="px-4 py-2 whitespace-nowrap">Lein Fee</th>
+            <th className="px-4 py-2 whitespace-nowrap">Total bill</th>
+            <th className="px-4 py-2 whitespace-nowrap">Total cost</th>
+            <th className="px-4 py-2 whitespace-nowrap">Net profit</th>
+            <th className="px-4 py-2 whitespace-nowrap">Investigator</th>
           </tr>
         </thead>
         <tbody>
           {/* Map over data and render rows */}
           {data.map((rowData, index) => (
             <tr key={index} className="text-center">
-              <td className="border px-4 py-2">{rowData.caseNumber}</td>
-              <td className="border px-4 py-2">{rowData.bsoDate}</td>
-              <td className="border px-4 py-2">{rowData.repoDate}</td>
-              <td className="border px-4 py-2">{rowData.bsoType}</td>
-              <td className="border px-4 py-2">{rowData.hitType}</td>
-              <td className="border px-4 py-2">{rowData.client}</td>
-              <td className="border px-4 py-2">{rowData.classification}</td>
+              <td className="border px-4 py-2 w-1/14">{rowData.caseNumber}</td>
+              <td className="border px-4 py-2 w-1/14">{rowData.bsoDate}</td>
+              <td className="border px-4 py-2 w-1/14">{rowData.repoDate}</td>
+              <td className="border px-4 py-2 w-1/14">{rowData.bsoType}</td>
+              <td className="border px-4 py-2 w-1/14">{rowData.hitType}</td>
+              <td className="border px-4 py-2 w-1/14">{rowData.client}</td>
+              <td className="border px-4 py-2 w-1/14">{rowData.classification}</td>
+              <td className="border px-4 py-2 w-1/14">{rowData.status}</td>
+              <td className="border px-4 py-2 w-1/14">{rowData.location}</td>
+              <td className="border px-4 py-2 w-1/14">{rowData.department}</td>
+              <td className="border px-4 py-2 w-1/14">{rowData.priority}</td>
+              <td className="border px-4 py-2 w-1/14">{rowData.assignedTo}</td>
+              <td className="border px-4 py-2 w-1/14">{rowData.resolvedDate}</td>
+              <td className="border px-4 py-2 w-1/14">{rowData.resolutionType}</td>
+              <td className="border px-4 py-2 w-1/14">{rowData.comments}</td>
+              <td className="border px-4 py-2 w-1/14">{rowData.client}</td>
             </tr>
           ))}
         </tbody>
